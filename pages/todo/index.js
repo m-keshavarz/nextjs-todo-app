b230bb78d5a675944ad940437804978c6d1a6af9
m-keshavarz <m.k.dev@outlook.com>
2021-10-20 16:51:28 +0330

    ssr

--- pages/todo/index.js
+++ pages/todo/index.js
@@ -50,7 +50,7 @@ export default function ToDo({ data }) {
   );
 }
 
-export async function getStaticProps() {
+export async function getServerSideProps() {
   const data = await axios.get(
     "https://616eaa7f715a630017b3975c.mockapi.io/tasks2"
   );
